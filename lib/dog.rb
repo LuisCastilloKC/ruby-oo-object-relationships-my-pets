9c62839e080a27e9532588202388f7ef5418779a
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-01-10 22:21:53 -0500

    commit tree 3d8d5d6d809efc07d883be5febfec4c8dd1cbbb0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,54 @@
 class Dog
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save()
+  end
+
+  def save
+    @@all << self 
+  end
+
+  def self.all
+    @@all
+  end
 end
+
+# Dog
+#   Instance methods
+#     initializes with a name and an Owner
+#     can change its owner
+#     can't change its name
+#     initializes with a nervous mood
+#     can change its mood
+#   Class methods
+#     knows all the dogs
+
+# Associations
+#   Owner instance methods
+#     #cats
+#       returnsa a collection of all the cats that belong to the owner
+#     #dogs
+#       returnsa a collection of all the dogs that belong to the owner
+#     #buy_cat
+#       can buy a cat that is an instance of the Cat class
+#       knows about its cats
+#     #buy_dog
+#       can buy a dog that is an instance of the Dog class
+#       knows about its dogs
+#     #walk_dogs
+#       walks the dogs which makes the dogs' moods happy
+#     #feed_cats
+#       feeds cats which makes the cats' moods happy
+#     #sell_pets
+#       can sell all its pets, which makes them nervous
+#       can sell all its pets, which leaves them without an owner
+#     #list_pets
+#       can list off its pets
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,90 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    save()
+  end
+
+  def save
+    @@all << self 
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    name = Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    name = Dog.new(name, self)
+  end
+
+  def walk_dogs
+    Dog.all.map {|dog| dog.owner == self ? dog.mood = "happy" : nil}
+  end
+
+  def feed_cats
+    Cat.all.map {|cat| cat.owner == self ? cat.mood = "happy" : nil}
+  end
+
+  def sell_pets
+    Dog.all.map do |dog| 
+      if dog.owner == self  
+        dog.mood = "nervous" 
+        dog.owner = nil 
+      end
+    end
+
+    Cat.all.map do |cat| 
+      if cat.owner == self  
+        cat.mood = "nervous" 
+        cat.owner = nil
+      end
+    end
+  end
+
+  def list_pets
+    "I have #{dogs().length} dog(s), and #{cats().length} cat(s)."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
+
+# Owner
+#   Instance methods
+#     #name
+#       can have a name
+#       cannot change owner's name
+#     #species
+#       initializes with species set to human
+#       can't change its species
+#     #say_species
+#       can say its species
+#   Class methods
+#     .all returns all instances of Owner that have been created
+#     .count returns the number of owners that have been created
+#     .reset_all can reset the owners that have been created
